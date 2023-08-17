795397e94b3ac609f27670f8853c1b4064a7a15d
José Marcel <josemarcel30@gmail.com>
2023-08-16 21:55:08 -0300

    Melhorando a Estrutura do Banco de Dados
    Melhorando a Estrutura do Banco de Dados e criando chave primaria.

--- curso_de_BD.sql
+++ curso_de_BD.sql
@@ -1,3 +1,18 @@
+
+
 CREATE DATABASE cadastro
 DEFAULT CHARACTER SET utf8mb4
 DEFAULT COLLATE utf8mb4_general_ci;
+
+
+create table pessoas(
+id int not null auto_increment,
+nome varchar (30) not null,
+nascimento date,
+sexo enum ('M', 'F'),
+peso decimal (5,2),
+altura decimal (3,2),
+nacionalidade varchar (20) default 'Brasil',
+primary key (id)
+)DEFAULT CHARACTER SET utf8mb4;
+
