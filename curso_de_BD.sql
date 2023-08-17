f0ee7c7fa8f032676e6fd0d75affe931caa6828c
José Marcel <josemarcel30@gmail.com>
2023-08-17 12:36:00 -0300

    Update curso_de_BD.sql

--- curso_de_BD.sql
+++ curso_de_BD.sql
@@ -1,27 +1,16 @@
-/*Inserindo Dados na Tabela (INSERT INTO)
+alter table pessoas
+drop column profissao;
 
-insert into pessoas
-(id, nome, nascimento, sexo, peso, altura, nacionalidade)
-values
-(default, 'carolina', '1987-11-19', 'F', '67.5', '1.54', default);
+alter table pessoas
+add column profissao varchar(30)  not null default'';
 
-select * from pessoas;  */
+alter table pessoas
+add column profissao varchar (30) not null default '' after nome ;
 
-/* Opção mais simples*/
-/* insert into pessoas values
-(default, 'Carlos', '1989-11-29', 'M', '97.5', '1.84', default); 
+alter table pessoas
+change column profissao prof varchar(30) not null default '' after nome ;
 
-select * from pessoas; */ 
+alter table pessoas
+rename to Usuario;
 
-/* Fazer varios cadstros de uma vez* usando insert into*/
-
-insert into pessoas 
-(id, nome, nascimento, sexo, peso, altura, nacionalidade)
-values
-(default, 'Camila', '1980-12-29', 'F', '87.5', '1.84', default),
-(default, 'João', '1949-05-29', 'M', '78.5', '1.74', default),
-(default, 'Amanda', '1956-07-29', 'F', '56.5', '1.64', 'Japão'),
-(default, 'Carlos', '1978-01-29', 'M', '99.5', '1.94', default),
-(default, 'Jumenta', '1999-09-29', 'F', '56.5', '1.54','Portugal');
-
-select * from pessoas; 
+select * from pessoas;
